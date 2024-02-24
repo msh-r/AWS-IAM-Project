9183184ae55a6a89d43d50d93cf8d701250e2550
msh-r <hramim389@gmail.com>
2024-02-23 20:01:16 -0500

    adding .csv and script-(updated)

--- aws-iam-create-user.sh
+++ aws-iam-create-user.sh
@@ -1,59 +1,50 @@
 #!/bin/bash
-#purpose : Automated user creation in the AWS
-#How to: ./aws-iam-create-user.sh <entry file format .csv>
-#Entry file column name: user, group, password
+# Purpose: Automated user creation in AWS
+# How to: ./aws-iam-create-user.sh <entry_file.csv>
+# Entry file column names: user, group, password
+# ------------------------------------------
+
+# Check if dos2unix is installed
+command -v dos2unix >/dev/null || { echo "Error: dos2unix tool not found. Please install dos2unix before running the script."; exit 1; }
 
 
+# Check if input file provided
+[ $# -ne 1 ] && { echo "Usage: $0 users.csv"; exit 1; }
 
-# Check if the correct number of arguments are provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 users.csv"
-    exit 1
-fi
+INPUT="$1"
 
-# Check if the input file exists
-if [ ! -f "$1" ]; then
-    echo "Error: Input CSV file '$1' not found."
-    exit 1
-fi
+# Check if input file exists
+[ ! -f "$INPUT" ] && { echo "Error: $INPUT file not found."; exit 1; }
 
-#Checking if dos2unix is present
-command -v dos2unix >/dev/null || {echo "dos2unix tool not found.Pleae install dos2unix tools before running the script."; exit 1; }
+# Convert input file to Unix format
+dos2unix "$INPUT"
 
-dos2unix $1
+# Read CSV file, create users, and assign them to groups
+# Reads from second line "tail -n +2 "$input"
 
-# Read the input CSV file line by line
-while IFS=',;' read -r username group password || [ -n "$user"]
- do
-    if [$user != "user" ]; then
-        # Check if username, group, and password are not empty
-        if [ -z "$user" ] || [ -z "$group" ] || [ -z "$password" ]; then
-            echo "Error: Invalid entry in CSV file. Skipping..."
-            continue
-        fi
+tail -n +2 "$INPUT" | while IFS=',;' read -r user group password; 
 
-        # Create IAM user
-        aws iam create-user --user-name "$user" >/dev/null 2>&1
-        if [ $? -eq 0 ]; then
-            echo "IAM user '$user' created successfully."
+do
 
-            # Add user to the specified group
-            aws iam add-user-to-group --user-name "$user" --group-name "$group" >/dev/null 2>&1
-            if [ $? -eq 0 ]; then
-                echo "IAM user '$user' added to group '$group' successfully."
-            else
-                echo "Error: Failed to add IAM user '$user' to group '$group'."
-            fi
-
-            # Set password for the user
-            aws iam create-login-profile --user-name "$user" --password "$password" >/dev/null 2>&1
-            if [ $? -eq 0 ]; then
-                echo "Password set successfully for IAM user '$user'."
-            else
-                echo "Error: Failed to set password for IAM user '$user'."
-            fi
-        else
-            echo "Error: Failed to create IAM user '$user'."
-        fi
+# Check if username, group, and password are not empty
+    if [ -z "$user" ] || [ -z "$group" ] || [ -z "$password" ]; then
+        echo "Error: Invalid entry in CSV file. Skipping..."
+        continue
     fi
-done < "$1"
+
+# Check if the user already exists
+    aws iam get-user --user-name "$user" >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "IAM user "$user" already exists. Skipping..."
+        continue
+    fi
+
+#If all condition checks then we create the user
+    aws iam create-user --user-name "$user"
+    aws iam create-login-profile --password-reset-required --user-name "$user" --password "$password"
+    aws iam add-user-to-group --group-name "$group" --user-name "$user"
+
+done
+
+
+echo "User creation completed."
